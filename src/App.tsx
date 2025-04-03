c86fe8995ed3b23946ed274bfc0261092daf24e6
Rohan-Munot <rohanmunot24@gmail.com>
2025-04-03 16:24:20 +0530

    Completed the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
-
+import Navbar from "./Components/Navbar";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="w-full relative">
+      <Navbar />
+      <div className="w-full h-screen relative">
+        <div className="w-full h-screen relative">
+          <div className="w-full h-screen relative">
+            <div className="w-full h-[1006px] relative">
+              <img
+                className="w-full h-[1006px] left-0 top-0 absolute"
+                src="/background.png"
+              />
+              <div className="w-full h-[839px] left-0 top-[167px] absolute bg-gradient-to-b from-white/0 via-white/30 to-white" />
+            </div>
+            <div className="mx-auto mt-[205px] w-full flex flex-col gap-8 justify-center items-center absolute top-0 ">
+              <div className="flex flex-col gap-6 relative ">
+                <h1 className="self-stretch text-center justify-start text-white text-[80px] font-normal font-['Alata'] leading-[84px] relative">
+                  The World's First Yogasana <br></br> Championship Software
+                  <img
+                    className="absolute right-10"
+                    src="/SoftwareVector.svg"
+                  />
+                </h1>
+                <p className="text-center justify-start text-[#f4f5f7] text-xl font-medium font-['Rethink_Sans'] leading-loose w-full">
+                  Experience the future of competition management with Yogasana
+                  Bharat—a cutting- <br></br>edge platform designed to
+                  streamline every aspect of your championship.
+                </p>
+              </div>
+              <button className="cursor-pointer h-[50px] px-5 py-4 bg-white rounded-[55px] shadow-[0px_0.75px_3.63px_rgba(89,108,148,0.04),0px_6px_29px_rgba(2,162,254,0.25)] inline-flex items-center text-right justify-center text-[#333f47] text-xl font-['Rethink_Sans'] font-semibold leading-normal">
+                Get Started for Free
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full mx-auto flex items-center justify-center absolute top-3/4">
+          <img src="main.png"></img>
+          <div className="w-full h-[964px] bg-gradient-to-b from-white/0 via-white/30 to-white absolute z-10 top-0" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,53 @@
+function Navbar() {
+  return (
+    <nav className="font-[Alata] flex text-sm absolute z-10 w-full items-center justify-center">
+      <div className="flex gap-[180px] w-[1320px] h-[60px] px-10 py-3 bg-white rounded-[111px] flex-row items-center mx-[60px] mt-12">
+        <span className="justify-center text-[#333f47] text-[28px] font-normal leading-normal">
+          YuGa
+        </span>
+        <div className="flex flex-row gap-[180px] w-full justify-center">
+          <div className="flex flex-row gap-10 justify-center items-center">
+            <a
+              href="#"
+              className="text-center justify-center text-[#333f47] text-base font-medium font-['Rethink_Sans'] leading-normal"
+            >
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-center justify-center text-[#333f47] text-base font-medium font-['Rethink_Sans'] leading-normal"
+            >
+              Solutions
+            </a>
+            <a
+              href="#"
+              className="text-center justify-center text-[#333f47] text-base font-medium font-['Rethink_Sans'] leading-normal"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-center justify-center text-[#333f47] text-base font-medium font-['Rethink_Sans'] leading-normal"
+            >
+              Pricing
+            </a>
+          </div>
+          <div className="flex flex-row gap-6">
+            <button className="cursor-pointer h-9 px-4 py-2 bg-[#222222] rounded-[444px] shadow-[0px_0.7512931227684021px_3.6312501430511475px_0px_rgba(89,108,148,0.04),0px_6px_29px_0px_rgba(2,162,254,0.25)] outline-1 outline-offset-[-0.50px] outline-[#dfe3e8] inline-flex justify-center items-center gap-2">
+              <div className="justify-start text-white text-sm font-semibold font-['Rethink_Sans'] leading-tight">
+                Talk To Sales
+              </div>
+            </button>
+            <button className="cursor-pointer h-9 px-4 py-2 rounded-[444px] shadow-[0px_5px_22.700000762939453px_0px_rgba(0,0,0,0.15)]  outline-1 outline-offset-[-0.50px] outline-[#333f47] inline-flex justify-center items-center gap-2">
+              <div className="justify-start text-[#333f47] text-sm font-semibold font-['Rethink_Sans'] leading-tight">
+                Get Started For Free
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
